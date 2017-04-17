6631619b8c2de7a9804d6b1491954f1fd08f6d7f
r.gavrilchenko <r.gavrilchenko@securitycode.ru>
2017-04-17 13:28:22 +0300

    init

---
+++ base.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python
+#coding: utf8
+import re
+import sys
+from datetime import datetime  
+from datetime import timedelta 
+
+Dni = ['PN', 'VT', 'SR', 'CT', 'PT', 'SB', 'ND']
+Minea = {}
+Triod = {}
+def Parse_M(text, god) :
+	mes = 0
+	den = None
+	slujba = None
+	dt = None
+	res = iter(re.findall(r'[A-Z\-+][A-Z0-9]+|"[^"\n]+"', text))
+	for t in res :   #new match
+		t = t.encode('utf-8')
+		t2 = t[0:2]
+		if t2 == 'DN' :
+			dt = datetime(day = int(t[2:]), month = mes, year = god) + timedelta(days=13)
+			if dt.year > god :
+				dt = datetime(day = dt.day, month = dt.month, year = god)
+		elif t2 == 'MS' :
+			mes = int(t[2:])
+		elif t2 in ['LT', 'VR', 'UT', 'ML', 'CH'] :					
+			den = Minea.setdefault("%i.%i" % (dt.day, dt.month), {})
+			slujba = den.setdefault(t, {})
+		elif t2 in ['MF','MK','LK','IN'] :
+			ev = slujba.setdefault('EV', [])
+			ev.append(t)
+		elif t2 == 'ZA' :						
+			den = Minea.setdefault("%i.%i" % (dt.day, dt.month), {})
+			slujba = den.setdefault('LT', {})			
+			ap = slujba.setdefault('AP', [])
+			ap.append(t)
+		if t[0] == '-' or t[0] == '+' :
+			if t[1:] in Dni :
+				while Dni[dt.weekday()] != t[1:] :
+					if t[0] == '-' :
+						dt -= timedelta(days = 1)
+					else :
+						dt += timedelta(days = 1)
+		elif t2 == 'IZ' or t2 == 'ZS':
+			den = Minea.setdefault("%i.%i" % (dt.day, dt.month), {})
+			add = den.setdefault('ADD', [])
+			add.append(t)			
+			pass
+		else :
+			pass
+
+
+def Parse_T(text) :
+	sedmica = None
+	den = None
+	slujba = None
+	res = re.findall(r'[A-Z][A-Z0-9]+|"[^"]+"', text) 
+	for t in res :   #new match
+		t = t.encode('utf-8')
+
+		t2 = t[0:2]
+		if t2 == 'SD' :		
+			sedmica = Triod.setdefault(t, {})
+			den = None
+			slujba = None
+		elif t2 in Dni :
+			den = sedmica.setdefault(t, {})
+			slujba = None
+		elif t2 in ['LT', 'VR', 'UT', 'ML', 'CH'] :
+			slujba = den.setdefault(t, {})
+		elif t2 == 'ZA' :
+			if slujba == None : slujba = den.setdefault('LT', {})
+			ap = slujba.setdefault('AP', [])
+			ap.append(t)
+		elif t2 in ['MF','MK','LK','IN', 'VE'] :
+			if slujba == None : slujba = den.setdefault('LT', {})
+			ev = slujba.setdefault('EV', [])
+			ev.append(t)
+		elif t2 == 'ZS' :
+			pass
+		else :
+			pass
+#		print t.decode('utf-8')
+
+def Load_M(fname, year) :
+	f = open(fname)	
+	m = Parse_M(f.read().decode('utf8'), year)
+	f.close()
+	return m
+
+def Load_T(fname) :
+	f = open(fname)	
+	m = Parse_T(f.read().decode('utf8'))
+	f.close()
+	return m
+
+def Raspisanie_Minei(date) :
+	
+	Load_M('ev_minea', date.year)
+	Load_M('ap_minea', date.year)
+	return Minea.get("%i.%i" % (date.day, date.month))
+
+def Raspisanie_Triodi(per, ned, den, sedm_po_Troici) :
+	VEV = ['MF116', 'MK70', 'MK71', 'LK112', 'LK113', 'LK114', 'IN63', 'IN64', 'IN65', 'IN66', 'IN67']
+
+	Load_T('ev_triod')
+	Load_T('ap_triod')
+	res = None
+	sedm = None
+	if per == 1 :
+		sedm = Triod.get('SDP' + str(ned))
+	elif per == 2 or per == 3 :
+		sedm = Triod.get('SDT' + str(ned))
+	elif per == 4 :		
+		sedm = Triod.get('SDV' + str(ned))
+
+	if sedm :
+		res = sedm.get(Dni[den]).copy() 
+
+		if Dni[den] == 'ND' and per != 1 :
+			ut = res.get('UT')		
+			if ut :
+				ev = ut.get('EV')			
+				if ev :
+					if 'VEV' in ev :
+						i = ev.index('VEV')
+						ev.pop(i)
+						evn = (sedm_po_Troici - 2) % 11
+						ev.insert(i, VEV[evn])
+
+	return res
+
+def GetFilesByMask(root, mask) :
+    res = []
+    tree = os.walk(root) 
+    for path, dirs, files in tree :
+       for fname in files :
+           if fnmatch.fnmatch(fname, mask) :
+               res.append(os.path.join(path, fname))
+    return res 
+
+#Load('ap_ot_paskhi.ld')
+#Load('ev_triod')
+#Load('ap_minea.ld')
+#Load('ev_minea')
+
+#print Minea
+if __name__ == "__main__" :         
+#	Minea = Load_M('ev_minea', 2016)
+#	print Minea
+		
+	Load_T('ev_triod')
+	print Triod
+	
+#	print Raspisanie_Minei(d)
+#	print Raspisanie_Paskhalii(4, 2, 2)
---
+++ ev.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python
+#coding: utf8
+#https://azbyka.ru/o-poryadke-cerkovnyx-chtenij-evangeliya
+#https://azbyka.ru/otstupka-i-prestupka
+#https://azbyka.ru/days/ukaz_cht.php
+#https://bogaiskov.ru/gospel-reading-arithmetic/
+
+import sys
+from datetime import datetime  
+from datetime import timedelta 
+import base
+
+Dni = [u'Пн', u'Вт', u'Cр', u'Чт', u'Пт', u'Сб', u'Вс']
+data_format = '%d.%m.%Y'
+date = datetime.strptime('27.06.1915', data_format)
+if len(sys.argv) > 1 : date = datetime.strptime(sys.argv[1], data_format)
+
+def SD(d) :
+	return d.strftime('%d.%m.%Y')
+
+def Paskha(y) :
+	a = y % 19
+	b = y % 4
+	c = y % 7
+	d = (19 * a + 15) % 30 
+	d += (2 * b + 4 * c + 6 * d + 6) % 7
+	if d < 9 :
+		return datetime(y, 3, 22 + d) + timedelta(days=13)
+	else :
+		return datetime(y, 4, d - 9) + timedelta(days=13)
+
+den_mes = date.day
+mes = date.month
+den_sedm = date.weekday()
+print u"Дата: " + SD(date) + ", " + Dni[den_sedm]
+
+god1 = date.year
+if (date - Paskha(god1)).days < 0 :	god1 -= 1
+
+PS1 = Paskha(god1)
+PS2 = Paskha(god1 + 1)
+Sedm_po_Paskhe = (date - PS1).days / 7 + 1
+Sedm_do_Paskhi = (PS2 - date).days / 7
+
+Troica = PS1 + timedelta(weeks = 7)
+Vozdv = datetime(god1, 9, 27)
+Kresch = datetime(god1, 1, 19)
+Sedm_po_Troici = (date - Troica).days / 7 + 1
+
+glas = (Sedm_po_Paskhe - 1) % 8
+if glas == 0 : glas = 8
+
+
+MrFr = PS2 - timedelta(weeks = 10)
+
+print u"Предыдущая Пасха: " + SD(PS1)
+print u"Троица: " + SD(Troica)
+print u"Следующая Пасха: " + SD(PS2)
+print u"Неделя о мытаре и фарисее: " + SD(MrFr)
+print u"Седмиц по Пасхе: " + str(Sedm_po_Paskhe)
+print u"Седмиц до Пасхи: " + str(Sedm_do_Paskhi)
+print u"Седмиц по Пятидесятнице: " + str(Sedm_po_Troici)
+print u"Глас седмицы: " + str(glas)
+
+#print Troica
+#print u"Период: " + str(Period)
+
+Luka = Vozdv
+while True :
+	Luka += timedelta(days = 1)      #"понедельник, следующий за этой Неделей, и есть искомый понедельник – начало Лукиных седмиц"
+	if (Luka.weekday() == 6) : break #Вс 
+Luka += timedelta(days = 1) #Пн
+sdvig_Luka = 18 - (Luka - Troica).days / 7 - 1
+Matf = Luka - timedelta(days = 3)
+if Vozdv.weekday() == 4 : #Пт
+	Matf -= timedelta(days = 1)		#"пяток по Воздвижении Креста указан здесь в качестве крайнего срока"
+Mf_Ots = Troica + timedelta(weeks = 17, days = 1) #Пн  
+Lk_Ots = Luka + timedelta(weeks = 16) #Пн
+Lk_Ots_sdvig = (MrFr - Lk_Ots + timedelta(days = 1)).days / 7
+
+#Лк: 35 из 30-й 29.01.2016
+
+print u"Конец Матфеева ряда: " + SD(Matf)
+#if Mf_Ots > 0 :
+#	print u"Начало Воздвиженской отступки: " + SD(Matf - timedelta(days = Mf_Ots))	
+if Mf_Ots < Matf :
+	print u"Начало Воздвиженской отступки: " + SD(Mf_Ots)	
+
+print u"Начало Лукина ряда: " + SD(Luka) #в Пн после Недели по Воздвижении
+print u"Сдвиг седмиц Лукина ряда: " + str(sdvig_Luka) #в Пн после Недели по Воздвижении
+print u"Начало Богоявленской отступки: " + SD(Lk_Ots)	
+
+print Lk_Ots_sdvig  
+
+#Особые:
+#МФ
+#Сб перед Воздв
+#Вс перед Воздв
+
+#Лк
+#Вс святых праотец
+#Вс святых отец, или Вс пред Рождеством Христовым, с Сб
+#Вс по Рождестве
+#Вс пред Богоявлением (Просвещением)
+#Вс по Богоявлении (Просвещении).
+#относительно Недели по Рождестве допускает возможность чтения ряда под зачало, но при условии «аще не будет отступки»
+
+#Если количество чтений Евангелия недостаточно, то в неделю по Рождестве
+#Христовом, в неделю перед Богоявлением и т.д. читаются
+#только особо предназначенные для этих недель Евангелия, а рядовые для этих недель Евангелия, равно как и рядовые, положенные в недели Праотец и Отец, ЧИТАЮТСЯ В ИЗБЫТОЧЕСТ¬ВУЮЩИЕ НЕДЕЛИ, но так, "чтобы в неделю, предшествующую мытаревой, было непременно Евангелие (32-й недоли) о Зак-хее" - Луки, зач. 94.
+#Если для избыточествующих недель недостает и этих за¬чал, то в неделю, предшествующую Закхеевой, читается зача¬ло 6-ое, от Матфея - о хананеянке (Евангелие 17-й недели).
+
+#празднование Собора новомучеников и исповедников Российских установлено совершать 25 января (при совпадении этого числа с воскресным днем) или в ближайшее воскресенье после него.
+#новомучеников (Рим., зач. 99).
+#новомучеников (Лк., зач. 105–106)
+
+def In() :
+	print "SDP" + str(Sedm_po_Paskhe) + " " + Dni[den_sedm]
+	print base.Raspisanie_Triodi(1, Sedm_po_Paskhe, den_sedm, Sedm_po_Troici)
+
+
+def Mf() :
+	sedm = Sedm_po_Troici
+	if sedm > 17 :
+		sedm = 10 + sedm - 17  #30.09.2015 - зачала Мф взяты из 11-й седмицы
+
+	print "SDT" + str(sedm) + " " + Dni[den_sedm]
+	print base.Raspisanie_Triodi(2, sedm, den_sedm, Sedm_po_Troici)
+
+
+def Lk() :
+	sedm = Sedm_po_Troici + sdvig_Luka
+	if date >= Lk_Ots :
+		sedm -= Lk_Ots_sdvig
+		if Lk_Ots_sdvig == 5 :    #[30, 31, 17, 32, 33]
+			if sedm == 29 or sedm == 30 : sedm += 1
+			elif sedm == 31 : sedm = 17 
+
+	print "SDT" + str(sedm) + " " + Dni[den_sedm]
+	print base.Raspisanie_Triodi(3, sedm, den_sedm, Sedm_po_Troici)
+
+def Tr() :
+	sedm = (date - MrFr).days / 7 + 1
+	print "SDV" + str(sedm) + " " + Dni[den_sedm]
+	print base.Raspisanie_Triodi(4, sedm, den_sedm, Sedm_po_Troici)
+
+
+if date < Troica : 		#До Пятидесятницы
+	In()
+elif date < Luka : 		#до Лукина ряда
+	Mf()
+elif date < MrFr : 		#до Недели о мытаре и фарисее
+	Lk()
+else :			   		#10-ть седмиц до Пасхи
+	Tr()
+
+print base.Raspisanie_Minei(date)
